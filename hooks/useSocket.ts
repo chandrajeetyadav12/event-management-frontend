ba82f9872c5f57d32c322fde210d2a3fc8008cc9
chandrajeetyadav12 <chandrajeet.yadav@crowdapps.in>
2026-07-07 11:35:29 +0530

    update socket  localhost url to env url

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -3,7 +3,10 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  "http://localhost:5000"
+  process.env.NEXT_PUBLIC_SOCKET_URL,
+  {
+    transports: ["websocket", "polling"],
+  }
 );
 
 export default socket;
